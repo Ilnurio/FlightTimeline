c98e1def70afdd448cb4e2988507a13b93fdd73c
Ilnur Fattakhov <nurich1995@mail.ru>
2023-06-21 20:34:50 +0300

    Initial Commit

---
+++ FlightTimeline/FlightTimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTimelineApp.swift
+//  FlightTimeline
+//
+//  Created by Ilnur on 21.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FlightTimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
